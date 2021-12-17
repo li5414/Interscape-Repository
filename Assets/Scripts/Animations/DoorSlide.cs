231b5d3d066152d82c5fdad1edc1b5dc6f9e0d2c
Jess Hammer <59108399+jess-hammer@users.noreply.github.com>
2021-12-17 10:18:19 +1000

    Started adding doors

---
+++ Assets/Scripts/Animations/DoorSlide.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorSlide : MonoBehaviour
+{
+    Animator anim;
+    Transform player;
+    public float distanceToOpen;
+
+    private bool isMoving;
+    private bool isOpen = false;
+
+    public AnimationClip doorSlideOpenClip;
+    public AnimationClip doorSlideCloseClip;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(player.position, transform.parent.transform.position) < distanceToOpen) {
+            if (!isMoving && !isOpen)
+                StartCoroutine("doorSlideOpen");
+        } else {
+            if (!isMoving && isOpen)
+                StartCoroutine("doorSlideClose");
+        }
+    }
+
+    private IEnumerator doorSlideOpen() {
+        anim.Play("DoorSlideOpen");
+        isMoving = true;
+        yield return new WaitForSeconds(doorSlideOpenClip.length);
+        isMoving = false;
+        isOpen = true;
+    }
+    private IEnumerator doorSlideClose() {
+        anim.Play("DoorSlideClose");
+        isMoving = true;
+        yield return new WaitForSeconds(doorSlideCloseClip.length);
+        isMoving = false;
+        isOpen = false;
+    }
+}
