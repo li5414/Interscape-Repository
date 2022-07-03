456b96b6fbca88298c10bcad43cc9bd0c3aa9430
jess-hammer <jrhammer50@gmail.com>
2022-07-03 10:57:07 +1000

    Add exit button script

---
+++ Assets/Scripts/MainMenu/ExitGame.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGame : MonoBehaviour {
+    public void ExitFunction() {
+        Application.Quit();
+    }
+}
